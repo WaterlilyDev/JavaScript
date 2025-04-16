91db8078f179b1130ee5cb4b9269849bdcf5b072
WaterlilyDev <trisha.singh5179@gmail.com>
2025-04-16 14:13:29 +0530

    job done

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,16 @@
-
-function upDate(previewPic){
+function upDate(preview_pic){
      // Get the div with id="image"
-     let imageDiv = document.getElementById("image");
-
-     // Update the background image URL to the source of the hovered thumbnail
-     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
- 
-     // Update the text inside the div to the alt text of the hovered thumbnail
-     imageDiv.innerHTML = previewPic.alt;
-    /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
-     
+     let image_div = document.getElementById("image");
+     //displaying the image hovered over
+     image_div.style.backgroundImage = "url('" + preview_pic.src + "')";
+     //displaying the text
+     image_div.innerHTML = preview_pic.alt;     
        }
-   
-       function unDo(){
-        // Get the div with id="image"
-    let imageDiv = document.getElementById("image");
-
-    // Revert the background image to its original state (e.g., none or default CSS styling)
-    imageDiv.style.backgroundImage = "";
-
-    // Revert the text to its original message
-    imageDiv.innerHTML = "Hover over an image below to display here.";
-        /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
        
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
-           
-       }
+function unDo(){
+    let image_div= document.getElementById("image");
+    //displaying original background
+    image_div.style.backgroundImage="";
+    //displaying orginal text
+    image_div.textContent="Hover over an image below to display here.";
+}  
