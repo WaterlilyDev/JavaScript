ff5bc81397762e6afd1c0b3dc39773e3f4a5bfab
WaterlilyDev <trisha.singh5179@gmail.com>
2025-04-27 17:40:16 +0530

    new folder made
    for the project

---
+++ picture_gallery/js/gallery.js
@@ -0,0 +1,16 @@
+function upDate(preview_pic){
+    // Get the div with id="image"
+    let image_div = document.getElementById("image");
+    //displaying the image hovered over
+    image_div.style.backgroundImage = "url('" + preview_pic.src + "')";
+    //displaying the text
+    image_div.innerHTML = preview_pic.alt;     
+      }
+      
+function unDo(){
+   let image_div= document.getElementById("image");
+   //displaying original background
+   image_div.style.backgroundImage="";
+   //displaying orginal text
+   image_div.textContent="Hover over an image below to display here.";
+}  
